ff547df09107b83f8d6ecb1c3b3cbb24212384d2
Kevin Colligan <kevincolligan@gmail.com>
2015-03-21 14:41:31 -0700

    Functional web browser

---
+++ BlocBrowser/BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by kevcol on 3/21/15.
+//  Copyright (c) 2015 kevcol inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface BLCWebBrowserViewController : ViewController
+
+@end
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by kevcol on 3/21/15.
+//  Copyright (c) 2015 kevcol inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
